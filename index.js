9fde836c041267a6fecf24701e1339c9bb92105a
demo73264-cyber <demo73264@gmail.com>
2026-05-05 07:51:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 
+console.log("BOT_TOKEN:", process.env.BOT_TOKEN);
+console.log("API_KEY:", process.env.API_KEY);
+console.log("Bot starting...");
+
 // ✅ ENV variables use karo
 const token = process.env.BOT_TOKEN;
 const API_KEY = process.env.API_KEY;
